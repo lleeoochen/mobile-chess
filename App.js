88d3651cd28be34e49487e2ced271a6b2d6977c1
Wei Tung Chen <weitungchen1998@gmail.com>
2020-06-18 23:37:41 -0700

    setup webview for mobile chess

---
+++ App.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { StatusBar, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar hidden={true} />
+        <WebView
+          source={{ uri: 'http://10.0.0.59:4000' }}
+          style={{ flex: 1 }}
+          userAgent="Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"/>
+      </View>
+    );
+  }
+}
+  
