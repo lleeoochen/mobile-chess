c090f81cedf5f81fd62e55cd54c5f00c918c1873
Wei Tung Chen <weitungchen1998@gmail.com>
2020-06-25 02:51:47 -0700

    enable native dev of react native

--- react-native/android/MainApplication.java
+++
@@ -1,8 +0,0 @@
-import com.github.alinz.reactnativewebviewbridge.WebViewBridgePackage;
-
-protected List<ReactPackage> getPackages() {
-    return Arrays.<ReactPackage>asList(
-        new MainReactPackage(),
-            new WebViewBridgePackage() //<- this
-    );
-}
